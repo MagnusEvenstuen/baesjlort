15fd2c91f1e96dc92a807e4fde20ff683727a538
Jon Aksel <jonakselm@gmail.com>
2026-03-24 11:04:58 +0100

    Fancy new logger launch with imu, remove whatever doesnt work

--- src/rov_bringup/launch/loggers.launch.py
+++ src/rov_bringup/launch/loggers.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushROSNamespace
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
@@ -9,32 +9,57 @@ def generate_launch_description():
     left_video_topic = LaunchConfiguration("left_video_topic")
     right_video_topic = LaunchConfiguration("right_video_topic")
 
-    left_video_logger = IncludeLaunchDescription(
-            PathJoinSubstitution([
-                FindPackageShare('log_to_file'),
-                'launch',
-                "video_logger.launch.py"
-                ]),
-                launch_arguments={
-                    "video_topic": left_video_topic
-                }.items()
+    imu_topic = LaunchConfiguration("imu_topic")
+
+    left_video_logger = GroupAction(
+            actions=[
+                PushROSNamespace("/gbr/cam_left"),
+                IncludeLaunchDescription(
+                    PathJoinSubstitution([
+                        FindPackageShare('log_to_file'),
+                        'launch',
+                        "video_logger.launch.py"
+                        ]),
+                    launch_arguments={
+                        "video_topic": left_video_topic
+                        }.items()
+                    )
+                ]
             )
 
-    right_video_logger = IncludeLaunchDescription(
+    right_video_logger = GroupAction(
+            actions=[
+                PushROSNamespace("/gbr/cam_right"),
+                IncludeLaunchDescription(
+                    PathJoinSubstitution([
+                        FindPackageShare('log_to_file'),
+                        'launch',
+                        "video_logger.launch.py"
+                        ]),
+                    launch_arguments={
+                        "video_topic": right_video_topic
+                        }.items()
+                    )
+                ]
+            )
+
+    imu_logger = IncludeLaunchDescription(
             PathJoinSubstitution([
                 FindPackageShare('log_to_file'),
                 'launch',
-                "video_logger.launch.py"
+                "imu_logger.launch.py"
                 ]),
-                launch_arguments={
-                    "video_topic": right_video_topic
+            launch_arguments={
+                "imu_topic": imu_topic
                 }.items()
             )
 
     # Return a launch description generated from node list
     return LaunchDescription([
-            DeclareLaunchArgument("left_video_topic", default_value="/gbr/cam_left/image_raw/compressed"),
-            DeclareLaunchArgument("right_video_topic", default_value="/gbr/cam_right/image_raw/compressed"),
+            DeclareLaunchArgument("left_video_topic", default_value="/gbr/cam_left/image_raw"),
+            DeclareLaunchArgument("right_video_topic", default_value="/gbr/cam_right/image_raw"),
+            DeclareLaunchArgument("imu_topic", default_value="/gbr/imu"),
             left_video_logger,
-            right_video_logger
+            right_video_logger,
+            imu_logger
         ])
