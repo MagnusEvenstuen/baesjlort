96b228fab2254b91f76abad692c7e9017f949591
Jon Aksel <jonakselm@tutanota.com>
2026-05-08 16:42:24 +0200

    Current thruster allocation matrix

--- src/ROV_controller/include/ROV_controller/ROV_controller.hpp
+++ src/ROV_controller/include/ROV_controller/ROV_controller.hpp
@@ -441,11 +441,11 @@ private:
     Eigen::Matrix<double, 8, 6> thrust_map_matrix = 
     (Eigen::Matrix<double, 8, 6>() <<
     -std::sqrt(2)/2,  std::sqrt(2)/2,  0.0,  0.0,              0.0,              0.156*std::sqrt(2),  // Thruster 1
-     std::sqrt(2)/2,  std::sqrt(2)/2,  0.0,  0.0,              0.0,             -0.156*std::sqrt(2),  // Thruster 2
+    -std::sqrt(2)/2, -std::sqrt(2)/2,  0.0,  0.0,              0.0,              0.156*std::sqrt(2),  // Thruster 2
     -std::sqrt(2)/2, -std::sqrt(2)/2,  0.0,  0.0,              0.0,             -0.156*std::sqrt(2),  // Thruster 3
-     std::sqrt(2)/2, -std::sqrt(2)/2,  0.0,  0.0,              0.0,              0.156*std::sqrt(2),  // Thruster 4
+    -std::sqrt(2)/2,  std::sqrt(2)/2,  0.0,  0.0,              0.0,             -0.156*std::sqrt(2),  // Thruster 4
      0.0,             0.0,            -1.0, -0.115,             0.096,            0.0,                 // Thruster 5
-     0.0,             0.0,            -1.0, -0.115,            -0.096,            0.0,                 // Thruster 6
+     0.0,             0.0,             1.0,  0.115,             0.096,            0.0,                 // Thruster 6
      0.0,             0.0,            -1.0,  0.115,             0.096,            0.0,                 // Thruster 7
      0.0,             0.0,            -1.0,  0.115,            -0.096,            0.0                  // Thruster 8
     ).finished();
